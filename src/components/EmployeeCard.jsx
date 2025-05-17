4d16daa6aafff561734cea71b1a43a1dab96c90d
Helder Balbino <helderbalbino@googlemail.com>
2025-05-17 13:44:27 +0100

    Implement rendering of employee image in EmployeeCard component

--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -1,3 +1,11 @@
 const EmployeeCard = ({ employee }) => {
-	return <div className='bg-white rounded-lg shadow p-4'></div>;
+	return (
+		<div className='bg-white rounded-lg shadow p-4'>
+			<img
+				src={employee.image}
+				alt={employee.name}
+				className='rounded-full w-24 h-24 mx-auto'
+			/>
+		</div>
+	);
 };
