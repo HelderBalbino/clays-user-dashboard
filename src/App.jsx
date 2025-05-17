39f3a770122f480754ce39eb83302fb8127f4d66
Helder Balbino <helderbalbino@googlemail.com>
2025-05-17 13:58:41 +0100

    Refactor App component to use Router and Routes for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 
 function App() {
 	return (
-		<div>
-			<Dashboard />
-		</div>
+		<Router>
+			<Routes>
+				<Route path='/' element={<Dashboard />} />
+			</Routes>
+		</Router>
 	);
 }
 
