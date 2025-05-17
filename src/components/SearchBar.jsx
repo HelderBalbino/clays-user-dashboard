019b43ae7b006ac8cbbbcbc07f2650c3025d78cb
Helder Balbino <helderbalbino@googlemail.com>
2025-05-17 14:08:57 +0100

    Refactor SearchBar component to use implicit return for cleaner syntax

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,10 @@
-const SearchBar = ({ setSearchTerm }) => {
-	return (
-		<input
-			type='text'
-			placeholder='Search by name'
-			onChange={(e) => setSearchTerm(e.target.value)}
-			className='p-2 border rounded w-full mt-4'
-		/>
-	);
-};
+const SearchBar = ({ setSearchTerm }) => (
+	<input
+		type='text'
+		placeholder='Search by name...'
+		onChange={(e) => setSearchTerm(e.target.value)}
+		className='p-2 border rounded w-full mt-4'
+	/>
+);
 
 export default SearchBar;
