2af075e4edeee32dc435838b74613ba260ffccd0
Helder Balbino <helderbalbino@googlemail.com>
2025-05-17 14:08:14 +0100

    Refactor EmployeeCard component to use implicit return for cleaner syntax

--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -1,18 +1,16 @@
-const EmployeeCard = ({ employee }) => {
-	return (
-		<div className='bg-white rounded-lg shadow p-4'>
-			<img
-				src={employee.image}
-				alt={employee.name}
-				className='rounded-full w-24 h-24 mx-auto'
-			/>
-			<h2 className='text-center mt-2 font-semibold'>{employee.name}</h2>
-			<p className='text-center text-sm'>{employee.role}</p>
-			<p className='text-center text-xs text-gray-500'>
-				{employee.department}
-			</p>
-		</div>
-	);
-};
+const EmployeeCard = ({ employee }) => (
+	<div className='bg-white rounded-lg shadow p-4'>
+		<img
+			src={employee.image}
+			alt={employee.name}
+			className='rounded-full w-24 h-24 mx-auto'
+		/>
+		<h2 className='text-center mt-2 font-semibold'>{employee.name}</h2>
+		<p className='text-center text-sm'>{employee.role}</p>
+		<p className='text-center text-xs text-gray-500'>
+			{employee.department}
+		</p>
+	</div>
+);
 
 export default EmployeeCard;
