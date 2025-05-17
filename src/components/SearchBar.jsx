279812744f14d8b23b917404186d937df669c195
Helder Balbino <helderbalbino@googlemail.com>
2025-05-17 13:54:02 +0100

    Refactor SearchBar component to include onChange handler and styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,3 +1,10 @@
 const SearchBar = ({ setSearchTerm }) => {
-	return <input type='text' placeholder='Search by name' />;
+	return (
+		<input
+			type='text'
+			placeholder='Search by name'
+			onChange={(e) => setSearchTerm(e.target.value)}
+			className='p-2 border rounded w-full mt-4'
+		/>
+	);
 };
